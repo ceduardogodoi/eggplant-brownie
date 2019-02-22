a965c7c1ff8e4b8fa7e898b89f3e4e833c4b743d
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-02-21 21:11:52 -0300

    Funções sem e com parâmetros e invocando funções.

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -5,3 +5,18 @@ let calories: Double = 50.5
 let happiness: Int = 5
 
 let eggplantIsAVegetable: Bool = false
+
+func helloCalories() {
+    let product = "brownie"
+    print("Hello Calories \(product)")
+}
+
+helloCalories()
+helloCalories()
+
+func add(name: String, calories: Double) {
+    print("adding \(name) with \(calories)")
+}
+
+add(name: "Brownie", calories: 30.5)
+add(name: "Eggplant", calories: 50.5)
