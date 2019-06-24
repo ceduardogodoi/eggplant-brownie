20fd209fc91f0d936d7993db46b8d073c7047686
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-06-24 00:32:51 -0300

    Good Citizen e criando classes e aplicativos que nunca quebram com nulos

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -3,17 +3,11 @@ import UIKit
 class Meal {
     var happiness: Int?
     var name: String?
+
+    init(name: String, happiness: Int) {
+        self.name = name
+        self.happiness = happiness
+    }
 }
 
-//var brownie = Meal()
-//brownie.name = "eggplant brownie"
-//
-//print(brownie.happiness)
-//print(brownie.name)
-//
-//if let name = brownie.name {
-//    print(name.uppercased())
-//}
-//
-//var name: String!
-//print(name.uppercased())
+let brownie = Meal(name: "eggplant brownie", happiness: 5)
